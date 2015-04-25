1940dc21c13e84aa05543c6a4395e04892305c6a
michiard <Pietro.Michiardi@eurecom.fr>
2015-04-25 15:24:58 +0200

    trying to get size right

--- src/main/scala/fr/eurecom/dsg/spark/TestDFSIO.scala
+++ src/main/scala/fr/eurecom/dsg/spark/TestDFSIO.scala
@@ -44,7 +44,7 @@ object TestDFSIO {
     	val a = sc.parallelize(1 until nFiles+1, nFiles)
 
       val b = a.map( i => {
-        val x = Array.ofDim[Char](fSizeBV.value).map(x => "0").mkString(" ")
+        val x = Array.ofDim[Char](fSizeBV.value / 2).map(x => "0").mkString(" ")
         x
       })
 
