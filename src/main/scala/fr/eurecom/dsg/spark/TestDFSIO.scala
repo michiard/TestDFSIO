2df551b39d943b6dddebe6caae5dae2aa56595c9
michiard <Pietro.Michiardi@eurecom.fr>
2015-04-25 15:39:46 +0200

    Fixing stat file + looking at read operation

--- src/main/scala/fr/eurecom/dsg/spark/TestDFSIO.scala
+++ src/main/scala/fr/eurecom/dsg/spark/TestDFSIO.scala
@@ -55,9 +55,9 @@ object TestDFSIO {
       // including that of creating the a, b RDDs and generating the data.
       // This means the measure is inaccurate, and the throughput will be smaller
     	val (junk, timeW) = profile {b.saveAsTextFile(ioFile)}
-    	statFile.write("\nTotal volume         : " + (nFiles * fSize) + "bytes")
-    	statFile.write("\nTotal write time     : " + (timeW/1000) + "s")
-      statFile.write("\nAggregate Throughput : " + (nFiles * fSize)/(timeW/1000) + "bytes per second")
+    	statFile.write("\nTotal volume         : " + (nFiles * fSize) + " Bytes")
+    	statFile.write("\nTotal write time     : " + (timeW/1000) + " s")
+      statFile.write("\nAggregate Throughput : " + (nFiles * fSize)/(timeW/1000) + " Bytes per second")
     	statFile.write("\n")
 	}
 
@@ -67,12 +67,12 @@ object TestDFSIO {
 	if (mode == "read") {
     	// Load file(s)
     	val b = sc.textFile(ioFile,nFiles)
-    	val (c, timeR) = profile {b.map(x => "0").max}
+    	val (c, timeR) = profile {b.map(x => "0").take(1)}
 
     	// Write stats
-    	statFile.write("\nTotal volume      : " + (nFiles * fSize) + "bytes")
-    	statFile.write("\nTotal read time   : " + (timeR/1000) + "s")
-      statFile.write("\nAggregate Throughput : " + (nFiles * fSize)/(timeR/1000) + "bytes per second")
+    	statFile.write("\nTotal volume      : " + (nFiles * fSize) + " Bytes")
+    	statFile.write("\nTotal read time   : " + (timeR/1000) + " s")
+      statFile.write("\nAggregate Throughput : " + (nFiles * fSize)/(timeR/1000) + " Bytes per second")
     	statFile.write("\n")
 	}
 
